e1df29e50807e42a310d74c568ef88c92c51b315
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-06-22 17:02:28 +0100

    example program

--- tree_traversal.py
+++ tree_traversal.py
@@ -1,7 +1,25 @@
-from test_trees import test_tree
-from utils import TreeNode
-from langchain_openai import ChatOpenAI
-import dotenv
+def traverse_tree(evidence, the_tree, llm, json_schema):
+    """Traverse the entire tree making calls to LLM."""
+    while the_tree.conclusion is None:
+        options = [answer for answer in the_tree.children]
+        structured_llm = llm.with_structured_output(json_schema(options))
 
-dotenv.load_dotenv()
-llm = ChatOpenAI(model="gpt-4o", temperature=0)
+        cur_prompt = f"""You are an expert in England and Wales law.
+        You are given the following case:
+
+        {evidence}
+
+        And must answer the following question:
+
+        {the_tree.question}
+        """
+
+        response = structured_llm.invoke(cur_prompt)
+
+        print(f"Question: {the_tree.question}")
+        print(f"LLM response: {response['decision']}")
+        print(f"LLM reasoning: {response['reasoning']}\n")
+
+        the_tree = the_tree.children[response['decision']]
+
+    print(f"Final conclusion: {the_tree.conclusion}\n")
